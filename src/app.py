16a26cd9bcb4adc73ecf2b8ff66cc05ff4d222e8
Nataliya <nataliya.kozlova@gmail.com>
2024-01-19 16:02:21 +0300

    upd

--- src/app.py
+++ src/app.py
@@ -16,18 +16,13 @@ server = app.server
 
 app.layout = html.Div([
             html.H1('Динамика тикера'),
-            dcc.Graph(id='my_graph',
-                        figure={'data':[
-                            {'x': [1,2], 'y':[3,1]}
-                        ], 'layout':{'title':'Default title'}}
-                ),
-            html.Div((html.H3('Введите тикер:', style={'paddingRight': '30px',  'padding-top': '0'}),
+            html.Div((html.H3('Введите тикер:', style={'paddingRight': '30px'}),
               dcc.Input(
                   id='my_stock_picker',
                   value='TSLA',
                   multiple=True,
                   style={'fontSize': 15, 'width': 75}
-              )), style={'display': 'grid', 'verticalAlign': 'top', 'color': 'blue', 'marginLeft': '10px'}),
+              )), style={'display': 'inline-block', 'verticalAlign': 'top', 'color': 'blue', 'marginLeft': '10px'}),
             html.Div([html.H3('Выберите период:'),
                       dcc.DatePickerRange(id='my_date_picker',
                                           min_date_allowed='2015-1-1',
@@ -35,15 +30,20 @@ app.layout = html.Div([
                                           start_date='2020-1-1',
                                           end_date=datetime.today()
                                           )
-                      ], style={'display':'grid', 'width': '30%', 'verticalAlign':'top', 'color': 'green',
-                                'marginLeft':'10px', 'padding-bottom': '2%'}),
+                      ], style={'width': '30%', 'color': 'green',
+                                'display':'inline-block', 'padding-bottom': '2%'}),
             html.Div([
                     html.Button(id='submit-button',
                                 n_clicks=0,
-                                children='Submit',
+                                children='OK',
                                 style={'fontSize': 15,'marginLeft':'10px', 'color':'white', 'color-scheme': 'dark',
-                                       'margin-bottom':'50px'})
-            ])
+                                       'margin-bottom':'10px'})
+            ]),
+            dcc.Graph(id='my_graph',
+                        figure={'data':[
+                            {'x': [1,2], 'y':[3,1]}
+                        ], 'layout':{'title':'Default title'}}
+                )
 ])
 
 @app.callback(Output('my_graph', 'figure'),
